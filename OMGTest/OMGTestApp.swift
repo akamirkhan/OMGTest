d515cf2f2ee6fa6ddc7eb9f636514643dd1761d5
Amirkhan <aak82183@gmail.com>
2024-03-03 10:38:43 +0300

    Initial Commit

---
+++ OMGTest/OMGTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMGTestApp.swift
+//  OMGTest
+//
+//  Created by Amirkhan Akaev on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct OMGTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
